856d60a255ea6bac80081d184ed5b46271b651b7
martin van der gragt <cbm@live.nl>
2017-11-15 22:18:08 +0100

    asd

--- code/resources/views/home/home.blade.php
+++ code/resources/views/home/home.blade.php
@@ -47,19 +47,101 @@
         hier onder projecten van studenten van Informatica Inholland
     </p>
     <div class="filters row">
-
+        <button class="filter__btn" data-action="cat1"><i class="fa fa-2x fa-gamepad"></i></button>
+        <button class="filter__btn" data-action="cat2"><i class="fa fa-2x fa-handshake-o"></i></button>
+        <button class="filter__btn" data-action="cat3"><i class="fa fa-2x fa-pinterest"></i></button>
     </div>
-    <div class="exhibit">
+    <div class="exhibit row">
         @for($i = 0; $i < 12; $i++)
-              <div class="exhibit__frame">
+              <div class="exhibit__frame col-4 col-sm-3 col-md-2 {{$i % 2 ==0 ? ($i % 3 == 0 ? "cat2": "cat3" ):"cat1"}}">
                   <a href="http://www.google.nl"><i class="fa fa-2x fa-search"></i></a>
                   <img class="exhibit__image" src="https://pbs.twimg.com/profile_images/848471660860538880/pevXVsIp.jpg">
               </div>
         @endfor
     </div>
 </div>
-<div id="events" class="events container-fluid">
+<div id="events" class="events  container-fluid">
+        @for($i = 0; $i < 2; $i++)
+            <div class="event__frame col-md-6 col-lg-6">
+                <div class="image__wrapper event__image" style="background-image: url('https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg');">
+                    <img width="100%" height="100%" src="https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg">
+                </div>
+                <div class="row">
+                    <div class="event__date col-sm-2">
+                        <h3>
+                            Apr
+                        </h3>
+                        <h4>
+                            22
+                        </h4>
+                        <hr>
+                        <h3>
+                            <i class="fa fa-heart"></i>
+                        </h3>
+                    </div>
+                    <div class="event__text col-sm-10">
+                        <h3>Title</h3>
+                        <h4>Created By Jane Doe</h4>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pretium libero sit amet tempus pretium. Suspendisse rutrum purus in ipsum dignissim, sit amet mollis sem vestibulum. Sed feugiat tellus at odio efficitur gravida. Sed nec tincidunt tellus, ac dapibus velit. Duis tincidunt leo libero, at ullamcorper arcu blandit ut. Nulla id eros malesuada, mollis purus sed, feugiat nibh. Nam aliquam eget lorem et faucibus.
+                        <br><a style="clear:both; float:left;" href="#"><i class="fa fa-arrow-right"></i> Lees meer</a>
+                    </div>
+                </div>
+            </div>
+        @endfor
+</div>
+<div id="usps" class="usps container-fluid">
+    <div class="row">
+       <div class="col-md-3" style="text-align: center">
+           <div class="usp">
+               <i class="fa fa-4x fa-university"></i>
+           </div>
+           <h4>Doorgroei mogelijkheden</h4>
+       </div>
+        <div  class="col-md-3" style="text-align: center">
+            <div class="usp">
+                <i class="fa fa-4x fa-graduation-cap"></i>
+            </div>
+            <h4>Erkende HBO diploma</h4>
+        </div>
+        <div class="col-md-3" style="text-align: center">
+            <div class="usp">
+                <i class="fa fa-4x fa-rocket"></i>
+            </div>
+            <h4>Leer de nieuwste technieken</h4>
+        </div>
+        <div class="col-md-3" style="text-align: center">
+            <div class="usp">
+                <i class="fa fa-4x fa-users"></i>
+            </div>
+            <h4>
+                De leerlingen staan centraal
+            </h4>
+        </div>
+    </div>
+</div>
+<div id="map" class="map container-fluid">
+    <div class="mapouter">
+        <div class="gmap_canvas">
+            <iframe width="100%" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=inholland haarlem&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+        </div>
+        <style>.mapouter{overflow:hidden;height:500px;width:100%;}.gmap_canvas {background:none!important;height:500px;width:100%;}</style></div>
+</div>
+<div id="overons" class="over_ons container">
+    <div class="row">
+        <div class="col-md-4">
+            <img width="100%" src="https://www.smashingmagazine.com/wp-content/uploads/2015/06/10-dithering-opt.jpg">
+        </div>
+        <div class="col-md-8">
 
+        </div>
+    </div>
+</div>
+<div id="contact" class="over_ons container">
+    <div class="row">
+        <div class="offset-md-2 col-md-8">
+
+        </div>
+    </div>
 </div>
 <script
         src="https://code.jquery.com/jquery-3.2.1.js"
@@ -78,6 +160,16 @@
             // when done, add hash to url
             // (default click behaviour)
             window.location.hash = this.hash;
+        });
+    });
+    $(".filter__btn").on("click",function(e){
+        e.preventDefault();
+        let filter = $(this).attr("data-action");
+        $('.exhibit__frame').each(function(){
+            $(this).show();
+        });
+        $('.exhibit__frame:not(.'+filter+')').each(function () {
+            $(this).hide();
         });
     });
 </script>
